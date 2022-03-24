93a08bd3565f172a58b1d0afe4ff89b89ff44633
Lukasamba <61199227+Lukasamba@users.noreply.github.com>
2022-03-25 01:32:19 +0200

    Added HTML

--- TPS/ClientApp/src/pages/Teams.vue
+++ TPS/ClientApp/src/pages/Teams.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>Hi this is Teams</h1>
+  <body>
+      <button>Sukurti komandą</button>
+      <table style="width:50%">
+        <tr>
+          <th>Team 1</th>
+          <th><button>Redaguoti</button></th>
+          </tr>
+        <tr><td>Planning 2022-03-21</td></tr>
+        <tr><td>Standup 2022-03-23</td></tr>
+        <tr><td>Standup 2022-03-24</td></tr>
+        <tr><td>Retro 2022-04-03</td></tr>
+        <tr>
+          <th>Team 2</th>
+          <th><button>Redaguoti</button></th>
+          </tr>
+        <tr><td>Planning 2022-03-21</td></tr>
+        <tr><td>Standup 2022-03-23</td></tr>
+        <tr><td>Standup 2022-03-24</td></tr>
+        <tr><td>Standup 2022-03-24</td></tr>
+        <tr><td>Retro 2022-04-03</td></tr>
+      </table>
+  </body>
 </template>
