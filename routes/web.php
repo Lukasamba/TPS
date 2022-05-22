000e3ce441a4ce8812b8bd17211975eac3888a9e
Lukasamba <lukasv093@gmail.com>
2022-05-22 18:59:35 +0300

    fixes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CalendarController;
+use App\Http\Controllers\ProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,11 +21,9 @@ Route::get('/', [MainController::class, 'openWelcomePage'])->name('welcomePage')
 Route::get('/home', [MainController::class, 'openHomePage'])->name('HomePage')->middleware('IsLogged');
 
 Route::get('/teams', [MainController::class, 'openTeamsPage'])->name('TeamsPage')->middleware('IsLogged');
-Route::get('/projects', [MainController::class, 'openProjectsPage'])->name('ProjectsPage')->middleware('IsLogged');
+Route::get('/projects', [ProjectController::class, 'openProjectsPage'])->name('ProjectsPage')->middleware('IsLogged');
 Route::get('/calendar', [CalendarController::class, 'openCalendarPage'])->name('CalendarPage')->middleware('IsLogged');
 
 Route::get('/signin', [AuthController::class, 'signin'])->name('signin')->middleware('AlreadyLogged');
 Route::get('/callback', [AuthController::class, 'callback'])->name('callback')->middleware('AlreadyLogged');
 Route::get('/signout', [AuthController::class, 'signout'])->name('signout')->middleware('IsLogged');
-
-Route::any('/sync', [AuthController::class, 'sync'])->name('sync')->middleware('IsLogged');
