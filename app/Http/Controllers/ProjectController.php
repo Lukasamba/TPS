e1515a1337713c3fc82ef766d6f3ba5e5bbee154
Deivydukas <deivis1221@gmail.com>
2022-06-17 20:41:23 +0300

    Project

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
+use Illuminate\Validation\ValidationException;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Illuminate\Support\MessageBag;
 use App\Models\Project;
 use App\Models\Team;
 use App\Models\User;
@@ -31,24 +34,30 @@ class ProjectController extends Controller
         static $allMyProjects = array();
 
         // Returns all projects
-        $allprojects = Project::all();
+        $allprojects = Project::all()->sortBy('projectName'); 
+
         // Get users id
         $userId = User::select(['userId'])->where('userEmail', session()->get('userEmail'))->exists();
-        // Teams by users id
+
+        // Get all teams with this users id
         $myTeams = DB::table('team_members')->where('fk_userId', $userId)->get();
+
+        // Get all project IDs from those teams
         foreach ($myTeams as $team) {
-            // Projects by my teams
-            $projectId = DB::table('team_projects')->where('fk_teamId', $team->fk_teamId)->value('fk_projectId');
+            $projectId = DB::table('team_projects')->where('fk_teamId', $team->fk_teamId)->get('fk_projectId');
             if (!is_null($projectId)) {
                 $projectIds = $projectIds->merge($projectId);
             }
         }
-        foreach ($projectIds as $id) {
-            $myprojects = DB::table('projects')->where('projectId', $id)->get()->first();
+
+        // Make project array
+        foreach ($projectIds as $pids) {
+            $myprojects = DB::table('projects')->where('projectId', $pids->fk_projectId)->get()->first();
             if (!is_null($myprojects)) {
                 array_push($allMyProjects, $myprojects);
             }
         }
+        $allMyProjects = collect($allMyProjects)->sortBy('projectName')->toArray();
         return view('projectspage', compact('allprojects'), compact('allMyProjects'));
     }
 
@@ -80,6 +89,11 @@ class ProjectController extends Controller
     // Inserts project into database
     public function insertProject(Request $request)
     {
+        $validated = $request->validate([
+            'projectName' => 'required|max:30',
+            'projectDescription' => 'required|max:255'
+        ]);
+
         $project = new Project;
         $project->projectName = $request->input('projectName');
         $project->projectDescription = $request->input('projectDescription');
