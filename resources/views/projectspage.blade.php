6a67c2314cdf568566b28b47158b085518f35e20
Just-Gud <just.gudai@gmail.com>
2022-06-20 14:46:02 +0300

    pataisyta rašyba

--- resources/views/projectspage.blade.php
+++ resources/views/projectspage.blade.php
@@ -94,7 +94,7 @@
             <td width="25%">
                 <a href="#" class="view" title="View" data-desc="{{$project->projectDescription}}" data-name="{{$project->projectName}}"" data-id=" {{$project->projectId}}" data-toggle="modal" data-target="#exampleModal">
                     <i class="material-icons">&#xE417;</i></a>
-                <a class="btn btn-primary" href="/eventGenerate/single/{{$project->projectId}}">Generuoti įvyki</a>
+                <a class="btn btn-primary" href="/eventGenerate/single/{{$project->projectId}}">Generuoti įvykį</a>
                 <a class="btn btn-primary" href="/eventGenerate/sprint/{{$project->projectId}}">Generuoti sprintą</a>
             </td>
         </tr>
